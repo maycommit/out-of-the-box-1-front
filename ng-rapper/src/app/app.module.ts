ee434da9e46cfc3fd406e0cb986fc328296bc44e
Tiago Souza <tiago.souza@zup.com.br>
2019-09-27 17:46:00 -0300

    Criar método post no html

--- ng-rapper/src/app/app.module.ts
+++ ng-rapper/src/app/app.module.ts
@@ -7,16 +7,18 @@ import { AppComponent } from './app.component';
 import { RappersComponent } from './rappers/rappers.component';
 import { RappersService} from './rappers.service';
 import { from } from 'rxjs';
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
+
   declarations: [
     AppComponent,
     RappersComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
-    
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [ RappersService ],
   bootstrap: [AppComponent]
